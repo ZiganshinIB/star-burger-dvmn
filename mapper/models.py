cb851617687e93b22e53b0817f0dc58751a6f125
ZiganshinIB <elzig3012@gmail.com>
2024-05-17 20:32:58 +0300

    Refract

--- mapper/models.py
+++ mapper/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Address(models.Model):
     address = models.CharField(max_length=256, unique=True, verbose_name='адрес', db_index=True)
-    lat = models.FloatField()
-    lng = models.FloatField()
+    lat = models.FloatField(null=True)
+    lng = models.FloatField(null=True)
 
 
     def __str__(self):
