882919d26eaeb645f80b4668553e1a7a78fa745e
ZiganshinIB <elzig3012@gmail.com>
2024-06-02 15:17:32 +0300

    Deploy without  input

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -3,10 +3,9 @@ set -e
 git pull
 /opt/star-burger-dvmn/.venv/bin/pip install -r requirements.txt
 /opt/star-burger-dvmn/.venv/bin/python manage.py migrate
-/opt/star-burger-dvmn/.venv/bin/python manage.py collectstatic
+/opt/star-burger-dvmn/.venv/bin/python manage.py collectstatic --no-input
 npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 systemctl disable star-burger
 systemctl reload star-burger
 systemctl enable star-burger
-
