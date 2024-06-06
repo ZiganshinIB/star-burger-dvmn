ec1f115ecd5f4feb2fbbd40fc50ae008b0aa2f75
ZiganshinIB_server <elzig3012@gmail.com>
2024-06-06 15:21:01 +0300

    Refract deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -8,6 +8,7 @@ revision=$(git rev-parse HEAD)
 npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 systemctl disable star-burger
-systemctl reload star-burger
+systemctl restart star-burger
 systemctl enable star-burger
+systemctl reload nginx
 curl -H "X-Rollbar-Access-Token: $ROLLBAR_ACCESS_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "production", "revision": "'$revision'", "rollbar_name": "'$ROLLBAR_NAME'", "local_username": "'$USER'", "comment": "Tuesday deployment", "status": "succeeded"}'
